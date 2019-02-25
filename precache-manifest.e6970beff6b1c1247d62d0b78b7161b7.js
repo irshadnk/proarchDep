2b64f5124a19db17b8a041ef838c2b3ec702ab05
Irshad <irshadnu@gmail.com>
2019-02-25 10:52:02 +0530

    Updates

---
+++ precache-manifest.e6970beff6b1c1247d62d0b78b7161b7.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "/proarchDep/static/media/notification.c0d3c94c.eot"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "/proarchDep/static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "/proarchDep/static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "/proarchDep/static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "1009db7b72f09c146ff8",
+    "url": "/proarchDep/static/js/runtime~main.1009db7b.js"
+  },
+  {
+    "revision": "4c01a0817fa833b20440",
+    "url": "/proarchDep/static/js/main.4c01a081.chunk.js"
+  },
+  {
+    "revision": "05cfc2d9d704964ee003",
+    "url": "/proarchDep/static/js/1.05cfc2d9.chunk.js"
+  },
+  {
+    "revision": "4c01a0817fa833b20440",
+    "url": "/proarchDep/static/css/main.715190f9.chunk.css"
+  },
+  {
+    "revision": "05cfc2d9d704964ee003",
+    "url": "/proarchDep/static/css/1.a0477d03.chunk.css"
+  },
+  {
+    "revision": "933390072b18e32d2b176c5a8e37a9ad",
+    "url": "/proarchDep/index.html"
+  }
+];
